6be57540741815f52f97231870d0d129dfa8edb4
Gabester0 <gceipper@gmail.com>
2021-02-03 23:54:45 -0500

    added a set method and made UserProps properties optional

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { User } from './models/Users';
 
 const user = new User({ name: 'haime', age: 22})
 
+user.set({ name: 'Nwnm', age: 990})
+user.set({ name: 'Nwnmee'})
+
 console.log(user.get('name'))
 console.log(user.get('age'))
