ec0e4287cf976a006020a9dbf5e21302e052172e
Gabester0 <gceipper@gmail.com>
2021-02-05 23:48:40 -0500

    added ability to add event handlers to our user object

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { User } from './models/Users';
 
 const user = new User({ name: 'haime', age: 22})
 
-user.set({ name: 'Nwnm', age: 990})
-user.set({ name: 'Nwnmee'})
+user.on('change', ()=>{});
+user.on('change', ()=>{})
+user.on('chuhchuhchanges', ()=>{})
 
-console.log(user.get('name'))
-console.log(user.get('age'))
+console.log(user)
