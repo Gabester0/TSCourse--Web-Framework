0cf472bf9ee1623680efc2482833b7098984f781
Gabester0 <gceipper@gmail.com>
2021-02-10 00:38:12 -0500

    testing triggering events

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import { User } from './models/User';
 const user = new User({name: 'I am not here', age: 3});
 
 user.save();
+
+user.events.on('change', ()=>{
+    console.log('Change!')
+});
+
+user.events.trigger('change')
