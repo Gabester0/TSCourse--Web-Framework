fb70158d248b3fcd88c73d74927f8580dab4324d
Gabester0 <gceipper@gmail.com>
2021-02-11 20:04:35 -0500

    exploring how to use current user model and future refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import { User } from './models/User';
 
 const user = new User({name: 'I am not here', age: 3});
-
-user.save();
-
-user.events.on('change', ()=>{
-    console.log('Change!')
-});
-
-user.events.trigger('change')
