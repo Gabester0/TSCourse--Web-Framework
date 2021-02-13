45f85ad5422b597d4da4e8ff70605debc7aa37a1
Gabester0 <gceipper@gmail.com>
2021-02-12 23:13:55 -0500

    implemented the sync fetch method in the User class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({name: 'I am not here', age: 3});
+const user = new User({ id: 1 });
 
 user.on('change', ()=> {
-    console.log('User was changed')
-})
+    console.log(user);
+});
 
-user.set({name: "Sam-bo"})
+user.fetch();
