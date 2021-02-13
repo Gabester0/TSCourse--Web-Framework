87325c77826030352be605ae884e83ecb0dca9bc
Gabester0 <gceipper@gmail.com>
2021-02-12 22:04:33 -0500

    updated getter to implement set()

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import { User } from './models/User';
 
 const user = new User({name: 'I am not here', age: 3});
 
-console.log(user.get('name'))
-
 user.on('change', ()=> {
     console.log('User was changed')
 })
 
-user.trigger('change')
+user.set({name: "Sam-bo"})
