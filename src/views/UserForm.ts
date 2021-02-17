1e6af1463feacb2c336128e1148943e90902e04a
Gabester0 <gceipper@gmail.com>
2021-02-16 23:23:49 -0500

    began first view Element

---
+++ src/views/UserForm.ts
@@ -0,0 +1,12 @@
+export class UserForm {
+    parent: Element
+
+    template(): string {
+        return `
+            <div>
+                <h1>User From</h1>
+                <input />
+            </div>
+        `
+    }
+}
