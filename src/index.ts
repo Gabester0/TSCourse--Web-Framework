8fa7c132b5b10a1939567a336b225a93ed9961a0
Gabester0 <gceipper@gmail.com>
2021-02-16 23:31:35 -0500

    rendering HTML on the page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { User } from './models/User'; 
+import { UserForm } from './views/UserForm';
 
-const collection = User.buildUserCollection();
+const root = document.getElementById('root');
+const userForm = new UserForm(root);
 
-collection.on('change', ()=>{
-    console.log(collection)
-})
-
-collection.fetch();
+userForm.render();
