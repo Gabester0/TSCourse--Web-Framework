7d959bfc91f02d5ac3fc5713424a372322a2552e
Gabester0 <gceipper@gmail.com>
2021-02-17 23:58:47 -0500

    implemented and tested solution

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -3,7 +3,8 @@ export class UserForm {
 
     eventsMap(): { [key:string]: ()=> void }{
         return {
-            'click:button': this.onButtonClick
+            'click:button': this.onButtonClick,
+            'mouseenter:h1': this.onHeaderHover
         };
     }
 
@@ -11,19 +12,36 @@ export class UserForm {
         console.log('Hi there');
     }
 
+    onHeaderHover(): void {
+        console.log('H1 was hovered over');
+    }
+
     template(): string {
         return `
             <div>
                 <h1>User Form</h1>
-                <button>Click Me! </button>
                 <input />
+                <button>Click Me!</button>
             </div>
         `
+    }
+
+    bindEvents(fragment: DocumentFragment): void {
+        const eventsMap = this.eventsMap();
+        for (let eventKey in eventsMap){
+            const [eventName, selector] = eventKey.split(':');
+            fragment.querySelectorAll(selector).forEach(element=>{
+                element.addEventListener(eventName, eventsMap[eventKey])
+            });
+        }
     }
 
     render(): void {
         const templateElement = document.createElement('template');
         templateElement.innerHTML = this.template();
+
+        this.bindEvents(templateElement.content)
+
         this.parent.append(templateElement.content)
     }
 }
