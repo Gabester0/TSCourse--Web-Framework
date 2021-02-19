2a703199c75d85aba066009e0c0f478db4237d34
Gabester0 <gceipper@gmail.com>
2021-02-18 23:46:06 -0500

    wiring up userform to show age and name of user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
+
+const user = User.buildUser({ name: "NAAAME", age: 34})
 
 const root = document.getElementById('root');
-const userForm = new UserForm(root);
+const userForm = new UserForm(root, user);
 
 userForm.render();
