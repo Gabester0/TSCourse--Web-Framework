d4b0ae19ec96b8eec0843c7560bd866472aa88ec
Gabester0 <gceipper@gmail.com>
2021-03-06 16:12:52 -0500

    set up regions and mapRegions methods)

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -5,7 +5,7 @@ export class UserEdit extends View<User, UserProps> {
     template(): string {
         return `
             <div>
-            <div class="user-show"></div>
+                <div class="user-show"></div>
                 <div class="user-form"></div>
             </div>
         `;
